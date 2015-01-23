4c962edcf71ee79e02fe197106f9ca3187f83554
Nick Collings <nick@collingo.com>
2015-01-23 15:44:09 +0000

    Render dynamic views on server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var App = require('express');
-var app = App();
+var cons = require('consolidate');
+
+// environment
 var env = process.env.ENV || 'development';
 var envConfig = {
 	development: {
@@ -13,8 +15,23 @@ var envConfig = {
 };
 var config = envConfig[env];
 
+// app setup
+var app = App();
+app.engine('html', cons.underscore);
+app.set('view engine', 'html');
+app.set('views', '../client');
+
+// dynamic routes
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+// static routes
+if(env === 'production') {
+	app.use('/client', App.static(__dirname + '/../client'));
+}
 app.use(App.static(__dirname + '/../' + config.clientDir));
 
+// go!
 app.listen(config.port);
-
 console.log('listening on port '+config.port);
